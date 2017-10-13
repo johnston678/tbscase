b84bdf95b57ef8041472b78f4d0bd813dcedb257
johnston678 <johnston678@qq.com>
2017-10-13 16:59:47 +0800

    Add files via upload

--- bug/jsapi.js
+++ bug/jsapi.js
@@ -1,3 +1,123 @@
+    //johnstonli
+    /**
+     * JSON stringify的实现
+     * @author norkts<norkts@gmail.com>
+     * @version 1.0 2015-11-24 15:11 实现了基本功能
+     * @version 1.1 2015-11-24 15:19 norkts 增加了JSON.stringify的兼用代码实现
+     * @version 1.2 2015-11-24 15:49 norkts 修改数组indexOf在IE下不兼容的写法,修改了undefined值的特殊处理
+     */
+    (function(NS){
+
+        //简单类型
+        var simpleTypes = ["number", "boolean", "undefined", "string", "function"];
+
+        //JSON.stringify的主函数
+        function stringify(object){
+            var type = typeof object;
+
+            //如果是简单类型，则直接返回简单类型的结果
+            if(indexOf(simpleTypes, type) > -1){
+                return parseSimpleObject(object);
+            }
+
+            //数组对象的
+            if(object instanceof Array){
+                var len = object.length;
+                var resArr = [];
+                for(var i = 0; i < len; i++){
+                    var itemType = typeof object[i];
+                    if(indexOf(simpleTypes, itemType) > -1){
+
+                        //undefined特殊处理，数组中变成null
+                        if(itemType !=  "undefined"){
+                            resArr.push(parseSimpleObject(object[i]));
+                        }else{
+                            resArr.push("null");
+                        }
+
+                    }else{
+                        //递归处理JS数组中的复杂元素
+                        resArr.push(stringify(object[i]));
+                    }
+                }
+
+                return "[" + resArr.join(",") + "]";
+            }
+
+            //普通object对象
+            if(object instanceof Object){
+                if(object == null){
+                    return "null";
+                }
+
+                var resArr = [];
+
+                for(var name in object){
+                    var itemType = typeof object[name];
+                    if(indexOf(simpleTypes, itemType) > -1){
+                        //undefined特殊处理，object中不编码
+                        if(itemType !=  "undefined"){
+                            resArr.push("\"" + name + "\":" + parseSimpleObject(object[name]));
+                        }
+                    }else{
+                        resArr.push("\"" + name + "\":" + stringify(object[name]));
+                    }
+                }
+
+                return "{" + resArr.join(",") + "}";
+            }
+        }
+
+        function parseSimpleObject(object){
+            var type = typeof object;
+            if(type == "string" || type == "function"){
+                return "\"" + object.toString().replace("\"", "\\\"") + "\"";
+            }
+
+            if(type == "number" || type == "boolean"){
+                return object.toString();
+            }
+
+            if(type == "undefined"){
+                return "undefined";
+            }
+
+            return "\"" + object.toString().replace("\"", "\\\"") + "\"";
+        }
+
+        function indexOf(arr, val){
+            for(var i = 0; i < arr.length; i++){
+                if(arr[i] === val){
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        /**
+         * 将stringify做二次封装
+         * @param object 要处理的对象
+         *
+         */
+        NS.stringify = function(object, isEncodeZh){
+            var res = stringify(object);
+            if(isEncodeZh){
+                var encodeRes = "";
+                for(var i = 0; i < res.length; i++){
+                    if(res.charCodeAt(i) < 0xff){
+                        encodeRes += res[i];
+                    }else{
+                        encodeRes += "\\u" + res.charCodeAt(i).toString(16);
+                    }
+                }
+                res = encodeRes;
+            }
+
+            return res;
+        };
+    })(window);
+
 var qq = function (e) {
     function l(t) {
         if (a[t])return a[t].exports;
@@ -5705,93 +5825,9 @@ var qq = function (e) {
             return "[null]" != O([e]) || "{}" != O({a: e}) || "{}" != O(Object(e))
         })), "JSON", {
         //johnstonli ES5 support JSON stringify
-
-    function parseSimpleObject(object){  
-        var type = typeof object;  
-        if(type == "string" || type == "function"){  
-            return "\"" + object.toString().replace("\"", "\\\"") + "\"";  
-        }  
-          
-        if(type == "number" || type == "boolean"){  
-            return object.toString();  
-        }  
-          
-        if(type == "undefined"){  
-            return "undefined";  
-        }  
-          
-        return "\"" + object.toString().replace("\"", "\\\"") + "\"";  
-    }  
-      
-    function indexOf(arr, val){  
-        for(var i = 0; i < arr.length; i++){  
-            if(arr[i] === val){  
-                return i;  
-            }  
-        }  
-          
-        return -1;  
-    }  
-    
-    //JSON.stringify的主函数  
-    stringify:function(object){  
-        var type = typeof object;  
-        var simpleTypes = ["number", "boolean", "undefined", "string", "function"];
-
-        //如果是简单类型，则直接返回简单类型的结果  
-        if(indexOf(simpleTypes, type) > -1){  
-            return parseSimpleObject(object);  
-        }  
-  
-        //数组对象的  
-        if(object instanceof Array){  
-            var len = object.length;  
-            var resArr = [];  
-            for(var i = 0; i < len; i++){  
-                var itemType = typeof object[i];  
-                if(indexOf(simpleTypes, itemType) > -1){  
-  
-                    //undefined特殊处理，数组中变成null  
-                    if(itemType !=  "undefined"){  
-                        resArr.push(parseSimpleObject(object[i]));  
-                    }else{  
-                        resArr.push("null");  
-                    }  
-                      
-                }else{  
-                    //递归处理JS数组中的复杂元素  
-                    resArr.push(stringify(object[i]));  
-                }  
-            }  
-              
-            return "[" + resArr.join(",") + "]";  
-        }  
-          
-        //普通object对象  
-        if(object instanceof Object){  
-            if(object == null){  
-                return "null";  
-            }  
-              
-            var resArr = [];  
-              
-            for(var name in object){  
-                var itemType = typeof object[name];  
-                if(indexOf(simpleTypes, itemType) > -1){  
-                    //undefined特殊处理，object中不编码  
-                    if(itemType !=  "undefined"){  
-                        resArr.push("\"" + name + "\":" + parseSimpleObject(object[name]));      
-                    }  
-                }else{  
-                    resArr.push("\"" + name + "\":" + stringify(object[name]));  
-                }  
-            }  
-              
-            return "{" + resArr.join(",") + "}";  
-        }  
-    }  
-      
-
+        stringify: function (e) {
+            return stringify(e,false);
+        }
         /*
         stringify: function (e) {
             if (void 0 !== e && !Y(e)) {
